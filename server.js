42a533a8a7960625e9adddc68084884d6aec8784
egeres <rafaleto@gmail.com>
2019-06-27 14:03:47 +0200

    system dynamically loads files from the chats folder

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
 const express    = require('express');
 const favicon    = require('express-favicon');
 const bodyParser = require('body-parser');
-
-
+const fs         = require('fs');
+const path       = require('path');
 
 // var React = require('react');
 // var ReactDOM = require('react-dom');
 // var App = require('./components/App')
 
-
+const dir = './chats';
 
 const port = 3001;
 const app = express();
@@ -31,12 +31,30 @@ app.post('/', (req, res) => {
 
 app.get('/datos', (req, res) => {
     // res.send('hello world 2');
-    res.send(
-        [
-            {nombre:"coso", datos_1:[0, 0, 10, 10, 20, 30]},
-            {nombre:"cosis",datos_1:[0, 0, 50, 60, 20, 30]}
-        ]
-    );
+
+    // console.log(
+    //     require('./chats/chat_0.json')
+    // );
+
+    // console.log(
+    //     fs.readdirSync(dir)
+    //         .filter(name => path.extname(name) === '.json')
+    //         .map(   name => require("./" + path.join(dir, name)))
+    // );
+
+    resultado = fs.readdirSync(dir)
+        .filter(name => path.extname(name) === '.json')
+        .map(   name => require("./" + path.join(dir, name)));
+
+
+    console.log(resultado);
+    res.send(resultado);
+    // res.send(
+    //     [
+    //         {nombre:"coso", datos_1:[0, 0, 10, 10, 20, 30]},
+    //         {nombre:"cosis",datos_1:[0, 0, 50, 60, 20, 30]}
+    //     ]
+    // );
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
--- src/index.jsx
+++ src/index.jsx
@@ -9,8 +9,12 @@ import * as d3 from 'd3';
 import chat_0 from '../chats/chat_0.json';
 
 
-console.log( chat_0 );
-console.log( chat_0[0].chat_messages_1 );
+
+let global_data = null;
+
+
+// console.log( chat_0 );
+// console.log( chat_0[0].chat_messages_1 );
 
 
 function Welcome(props) {
@@ -36,8 +40,36 @@ class Contacts extends React.Component {
     }
 
     componentDidMount() {
-        // console.log("---");
-        console.log(this.props);
+        // console.log("vvv");
+        // console.log(this.props);
+        // console.log( global_data );
+
+        if (this.props.my_index || (this.props.my_index === 0)) {
+            // console.log(this.props.my_index);
+            // console.log( global_data[this.props.my_index] );
+            // console.log( global_data[this.props.my_index] );
+
+
+            var rect_width = 4;
+
+            d3.select(this.refs.waveGroup).selectAll('path')
+            // .data(json_data["data"])
+                // .data([0, 20, 0,0,0,0,0,50,12])
+                // .data(chat_0[0].chat_messages_1)
+                .data(global_data[this.props.my_index].datos_1)
+                .enter().append("rect")
+                .attr("x"      , function(d, i) { return i*(rect_width) }  )
+                .attr("y"      , function(d, i) { return 0 }  )
+                .attr("width"  , function(d, i) { return (rect_width)       }  )
+                .attr("height" , function(d, i) { return 120 }  )
+                // .attr("fill"   , function(d, i) { return d3.interpolateYlOrRd( 1-(d/json_data["timeline_float_max"]) )     }  );
+                .attr("fill"   , function(d, i) { return d3.interpolateYlOrRd( 1-(d/50) )     }  );
+
+
+            // console.log("^^^");
+        }
+
+
 
         // d3.select(this.refs.waveGroup).append("circle")
         //     .attr("fill",  "red")
@@ -45,20 +77,7 @@ class Contacts extends React.Component {
         //     .attr("cy", 30)
         //     .attr("r", 20);
 
-        var rect_width = 4;
 
-        d3.select(this.refs.waveGroup).selectAll('path')
-        // .data(json_data["data"])
-          // .data([0, 20, 0,0,0,0,0,50,12])
-          // .data(chat_0[0].chat_messages_1)
-          .data(chat_0[0].chat_messages_1)
-          .enter().append("rect")
-          .attr("x"      , function(d, i) { return i*(rect_width) }  )
-          .attr("y"      , function(d, i) { return 0 }  )
-          .attr("width"  , function(d, i) { return (rect_width)       }  )
-          .attr("height" , function(d, i) { return 120 }  )
-          // .attr("fill"   , function(d, i) { return d3.interpolateYlOrRd( 1-(d/json_data["timeline_float_max"]) )     }  );
-          .attr("fill"   , function(d, i) { return d3.interpolateYlOrRd( 1-(d/50) )     }  );
     }
 
     render() {
@@ -109,7 +128,7 @@ class Contacts extends React.Component {
 //     ];
 // }
 
-var global_data = null;
+// var global_data = null;
 
 class App extends React.Component {
 // let App = React.createClass({
@@ -122,21 +141,72 @@ class App extends React.Component {
   //   return images.map(this.createImage);
   // },
 
+    constructor (props){
+        super(props);
+        this.state = {
+            // loopActive: false,
+            // shuffleActive: false,
+        };
+        this.create_graph_i = this.create_graph_i.bind(this);
+        this.create_fetch   = this.create_fetch.bind(this);
+        this.create_graph   = this.create_graph.bind(this);
+        this.create_graphs   = this.create_graphs.bind(this);
+    }
 
     create_graph_i(my_index) {
         return <Contacts my_index={my_index}/>;
     }
 
-    // create_graph(file_individual) {
-    //     return <Contacts file={file_individual}/>;
-    // }
+    create_fetch(in_this) {
 
-    // create_graphs(files)
-    // {
-    //     return files.map(this.create_graph);
-    // }
+        console.log("....");
+
+        var global_this = this;
+
+        // fetch('/datos')
+
+
+        console.log (
+
+        fetch('/datos')
+        // .then(function(response) { return response.text(); })
+        .then(function(response) { return response.json(); })
+        .then(function(data)     {
+
+            console.log('data = ');
+            console.log(data);
+
+            global_data = data;
+            var array_a = [];
+
+
+            for (var i = 0; i < data.length; i++) {
+                // this.create_graph_i(i);
+                console.log(":");
+                // array_a.push(this.create_graph_i(i));
+                // array_a.push(create_graph_i(i));
+            }
+
+            // return array_a;
+            return <h1>as f ff dasd</h1>;
+            // return in_this.create_graph_i(i);
+            // return ["aaaa", "nnnn"].map(global_this.create_graph);
+        })
+
+        );
+        return <h1>asdasd</h1>;
+    }
+
+    create_graph(file_individual) {
+        return <Contacts file={file_individual}/>;
+    }
+
+    create_graphs(files) {
+        return files.map(this.create_graph);
+    }
 
     render() {
+
         return [
             (<div id="aaa" class="container_info">
                 <div class="graph_title">Nanai</div>
@@ -145,7 +215,8 @@ class App extends React.Component {
             </div>),
 
             (<div id="bbb" class="container_graphs">
-                // {this.create_graphs(["graph_0", "graph_1", "graph_2"])}
+                { this.create_graphs(["graph_0", "graph_1", "graph_2"]) }
+                { this.create_fetch() }
             </div>)
         ];
     }
@@ -155,26 +226,26 @@ class App extends React.Component {
 
         // fetch('localhost:3001', {method:'post'}) // <-- this path surprises me
         //   .then(response => function() { console.log(response); })
-
-        fetch('/datos')
-        .then(function(response) { return response.text(); })
-        .then(function(data)     {
-            console.log('data = ', data);
-            global_data = data;
-            // data.map(this.create_graph);
-            for (var i = 0; i < data.length; i++) {
-                // array[i]
-                this.create_graph_i(i);
-            }
-
-        });
+        //
+        // fetch('/datos')
+        // .then(function(response) { return response.text(); })
+        // .then(function(data)     {
+        //     console.log('data = ', data);
+        //     global_data = data;
+        //     // data.map(this.create_graph);
+        //     for (var i = 0; i < data.length; i++) {
+        //         // array[i]
+        //         this.create_graph_i(i);
+        //     }
+        //
+        // });
 
 
     }
 
 }
 
-
+console.log(0);
 
 let general_0 = ReactDOM.render( <App/>, document.getElementById('root') );
 // var contenido_generado_a = ReactDOM.render( <Contacts file="chat_0"/>, document.getElementById('bbb') );
@@ -182,6 +253,50 @@ let general_0 = ReactDOM.render( <App/>, document.getElementById('root') );
 // var contenido_generado_c = ReactDOM.render( <Contacts file="chat_2"/>, document.getElementById('bbb') );
 
 // console.log(contenido_generado);
+
+
+
+
+// setTimeout(
+//     function() {
+//         var children = [];
+//         for (var i = 0; i < 10; i += 1) {
+//             children.push(<Contacts/>);
+//         };
+//         ReactDOM.render( children, document.getElementById('bbb') );
+//     }, 1000
+// );
+
+
+
+fetch('/datos')
+    // .then(function(response) { return response.text(); })
+    .then(function(response) { return response.json(); })
+    .then(function(data)     {
+
+        console.log('data = ');
+        console.log(data);
+
+        global_data = data;
+
+        var children = [];
+
+        for (var i = 0; i < data.length; i++) {
+            // this.create_graph_i(i);
+            console.log(":");
+            // array_a.push(this.create_graph_i(i));
+            // array_a.push(create_graph_i(i));
+            children.push(<Contacts my_index={i} />);
+        }
+        ReactDOM.render( children, document.getElementById('bbb') );
+
+
+    });
+
+
+
+
+
 
 
 
